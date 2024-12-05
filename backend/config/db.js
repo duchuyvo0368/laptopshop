4c7ae495e92a872c612df1e21f339dcf07202024
duchuyvo0368 <voduchuy016688@gmail.com>
2024-12-05 21:18:01 +0700

    fix database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,8 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
+      useUnifiedTopology: true,
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
