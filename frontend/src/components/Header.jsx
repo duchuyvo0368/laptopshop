5a17a63c7f83cfd9084f162490c4101dbe89c81c
duchuyvo0368 <voduchuy016688@gmail.com>
2024-12-06 00:17:04 +0700

    fix logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -35,8 +35,7 @@ const Header = () => {
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to='/'>
-            <img src={logo} alt='ProShop' />
-            ProShop
+            Laptopshop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
